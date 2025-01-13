cbe847cf5a06ca4f9a60f440b7b3269ade67dc38
Andrii P <andriip@MAC-1F9716.local>
2025-01-13 14:30:44 +0100

    done

--- src/main/java/com/example/demo/controller/PingController.java
+++ src/main/java/com/example/demo/controller/PingController.java
@@ -5,4 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
+    @GetMapping("test-link/ping")
+    public String ping() {
+        return "pong";
+    }
 }
