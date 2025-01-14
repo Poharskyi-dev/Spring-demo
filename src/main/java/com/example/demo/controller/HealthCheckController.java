945b67c2e0c538ec7878dcd8ba3a421d54a6469f
Andrii P <andriip@MacBook-Pro-Andrii.local>
2025-01-14 20:21:31 +0100

    User POST and GET methods with API v2

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -1,4 +1,18 @@
 package com.example.demo.controller;
 
-public class HealthCheck {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("The server is up and running");
+    }
+
 }
--- src/main/java/com/example/demo/controller/dto/CreateUserDto.java
+++ src/main/java/com/example/demo/controller/dto/CreateUserDto.java
@@ -1,4 +1,52 @@
 package com.example.demo.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CreateUserDto {
+
+    private String firstName;
+    private String email;
+    private String password;
+
+    public CreateUserDto() {
+    }
+
+    public CreateUserDto(String password, String email, String firstName) {
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateUserDto{" +
+                "firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
